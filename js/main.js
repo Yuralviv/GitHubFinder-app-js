6370b7144b106ce0246d5cabc499dbe14a87d64f
Yuriy <ivanivyura@gmail.com>
2019-03-25 09:45:04 +0200

    git init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+//  Fetch
+// const server = new JSONpalceholder();
+// server.getCommerntsByID(1)
+// .then(res => data.id)
+// .then(server.getCommerntsByID)
+// .then(comment => console.log(comments));
+//
